177d7a2f891459fdbc3dc945bb571df2e478e13a
seanmcculloch <sean.mcculloch@alleninstitute.org>
2023-09-19 13:12:20 -0700

    da.array compatibility to create_zarr

--- bigstream/utility.py
+++ bigstream/utility.py
@@ -556,10 +556,11 @@ def numpy_to_zarr(array, chunks, path):
     if not isinstance(array, zarr.Array):
         
         if isinstance(array, da.Array):
-            zarr_disk = create_zarr(path, array.shape, chunks, array.dtype, multithreaded=True)
-
+            store = zarr.DirectoryStore('data/array.zarr')
             print("converting dask array to zarr")
+            zarr_disk = zarr.array(None, shape=array.shape, chunks=chunks, dtype=array.dtype, store=store, overwrite=True)
             da.to_zarr(array, zarr_disk)
+
         else:
             zarr_disk = create_zarr(path, array.shape, chunks, array.dtype)
 
